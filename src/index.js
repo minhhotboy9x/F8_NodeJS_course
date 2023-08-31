2acb1c7cb01a2207fb21a15adfcbe730764ad596
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2023-08-31 15:56:10 +0700

    test

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.engine(
     }),
 );
 
-app.set('view engine',    'hbs');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // home, search, contact
@@ -31,6 +31,6 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // route init
 route(app);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+                app.listen(port, () => {
+                    console.log(`Example app listening on port ${port}`);
+                });
